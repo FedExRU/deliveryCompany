b109232cac62244c311b018339be944ddb4302ca
Урядышев Павел <uruadyshev@energyhouse.ru>
2018-07-17 17:43:39 +0300

    add catalog html layout and scss styles. Described index method (action) of CatalogController. Suppliers and Goods models was created. Setting up model's relationships one-to-many between Suppliers and Goods. Pagination was created v.2

--- resources/views/layout/part/sale-button.blade.php
+++ resources/views/layout/part/sale-button.blade.php
@@ -1,6 +1,6 @@
 <a class="btn-sale {{ $type }}" href="{{ $url }}">
 	<div class="row">
-		<div class="col-1">
+		<div class="col-2">
 			<i class="{{ $iconClass }} sale-icon"></i>
 		</div>
 		<div class="col-10">
